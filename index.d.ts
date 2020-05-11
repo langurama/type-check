d58147eca6c36633b221fbc5288f82741eec58cb
Karl Morrison <karl@basickarl.io>
2020-05-11 10:50:43 +0200

    Fixed retarded TypeScript.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module '@langurama/type-check' {
-    export default {
+    interface LanguramaTypeCheck {
         array: (object: any) => boolean;
         asyncFunction: (object: any) => boolean;
         bigint: (object: any) => boolean;
@@ -17,4 +17,39 @@ declare module '@langurama/type-check' {
         symbol: (object: any) => boolean;
         undefined: (object: any) => boolean;
     }
+
+    export const languramaTypeCheck: LanguramaTypeCheck;
+
+    export default languramaTypeCheck;
 }
+
+// declare module '@langurama/log' {
+//     interface LanguramaTerminalConfiguration {
+//         type: 'terminal';
+//         level?: 'error' | 'warn' | 'info' | 'debug' | 'trace';
+//         callee?: boolean;
+//         chalk?: Object;
+//     }
+
+//     interface LanguramaFileConfiguration {
+//         type: 'file';
+//         level?: 'error' | 'warn' | 'info' | 'debug' | 'trace';
+//         callee?: boolean;
+//         path?: string;
+//         json?: boolean;
+//     }
+
+//     type LanguramaConfiguration = LanguramaTerminalConfiguration | LanguramaFileConfiguration;
+
+//     interface LanguramaLog {
+//         error: (...messages: any) => void;
+//         warn: (...messages: any) => void;
+//         info: (...messages: any) => void;
+//         debug: (...messages: any) => void;
+//         trace: (...messages: any) => void;
+//     }
+
+//     export default function languramaLog(
+//         configuration?: LanguramaConfiguration | LanguramaConfiguration[]
+//     ): LanguramaLog;
+// }
